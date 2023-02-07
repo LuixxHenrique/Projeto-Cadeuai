1b96e881824f8cbcb1012f4e0fc5b00ec9b697c5
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-02-07 00:43:01 -0300

    dando merda v1

--- CADEUAI/src/app/app.module.ts
+++ CADEUAI/src/app/app.module.ts
@@ -9,11 +9,22 @@ import { AppComponent } from './app.component';
 
 import { IonicStorageModule } from '@ionic/storage-angular';
 
-import { Camera, CameraResultType } from '@capacitor/camera';
+import { AngularFireModule } from 'angularfire2' 
+import { AngularFireDatabase } from 'angularfire2/database'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot(),
+    AngularFireModule.initializeApp({
+    apiKey: "AIzaSyCjLBB8b26K8OCXPPm0cW8gyrXAYoLsSFg",
+    authDomain: "cadeuai-project.firebaseapp.com",
+    projectId: "cadeuai-project",
+    storageBucket: "cadeuai-project.appspot.com",
+    messagingSenderId: "776907303040",
+    appId: "1:776907303040:web:abf54785ec5d085e389e65",
+    measurementId: "G-5K1N0N2C50"}),
+    AngularFireDatabase
+  ],
   providers: [ { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
