4fb0496eb691d30fc95bc4308d686a0d91d3b69e
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-03-08 16:49:28 -0300

    ajustemo

--- skt/src/app/home/home.page.ts
+++ skt/src/app/home/home.page.ts
@@ -5,6 +5,7 @@ import { Camera, CameraResultType } from '@capacitor/camera';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { MapGenerationService } from '../services/map-generation.service';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -16,7 +17,8 @@ export class HomePage implements OnInit{
   
   constructor(
     private route : ActivatedRoute,
-    private mapgenerate: MapGenerationService
+    private mapgenerate: MapGenerationService,
+    private modalController: ModalController
   ) {}
 
   ngOnInit() {
@@ -24,23 +26,24 @@ export class HomePage implements OnInit{
     var id: any = String(this.route.snapshot.paramMap.get('id'))
     this.image_path = `assets/map-shopping/entry-${id}/entry${id}-inicio-${id}.png`
 
-    // this.map_css(1.5)
+    this.map_css(1.5)
   }
 
   routeSelect(loja: string){
     // coleta o id da pagina e monstra a imagem da rota até o setor
+    this.modalController.dismiss()
     var id: any = String(this.route.snapshot.paramMap.get('id'))
     this.image_path = `assets/map-shopping/entry-${id}/entry${id}-${loja}.png`
   }
-  // map_css(scale: number){
-  //   var imgMap = document.getElementById('img_map')
-  //   if (imgMap) {
-  //     const scaleString = `scale(${scale})`
-  //     const rotate = "rotate(-90deg)"
-  //     imgMap.style.transform = `${scaleString} ${rotate}`
-  //     imgMap.style.marginTop = "7.5rem"
-  //   }
-  // }
+  map_css(scale: number){
+    var imgMap = document.getElementById('img_map')
+    if (imgMap) {
+      const scaleString = `scale(${scale})`
+      const rotate = "rotate(-90deg)"
+      imgMap.style.transform = `${scaleString} ${rotate}`
+      imgMap.style.marginTop = "9rem"
+    }
+  }
   
   // showImage() {
   //   const image = document.querySelector('ion-img');
